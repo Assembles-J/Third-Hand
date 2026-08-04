9cd363264b651b38822212ad16452bb855f6b05e
王鹏 <pengpeng_on@163.com>
2026-08-04 14:55:03 +0800

    feat: 添加 android依赖

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
