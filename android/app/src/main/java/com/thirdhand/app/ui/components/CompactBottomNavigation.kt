1275a9fccc1669348c194db9cb1e98238f4f78fa
Peng <pengpeng_on@163.com>
2026-09-02 20:38:16 +0800

    fix(android): compile exact-height stock detail navigation

--- android/app/src/main/java/com/thirdhand/app/ui/components/CompactBottomNavigation.kt
+++ android/app/src/main/java/com/thirdhand/app/ui/components/CompactBottomNavigation.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
